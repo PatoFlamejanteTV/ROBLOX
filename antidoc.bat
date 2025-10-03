4a50fecad1e4ddefaf722cfaec3312f5bdc29292
UltimateQuack <patoflamejantetv@gmail.com>
2025-10-03 16:28:48 -0300

    DONT BRICK WINDOWS UPDATE

--- antidoc.bat
+++ antidoc.bat
@@ -1,9 +1,14 @@
 @echo off
-REM Deletes all folders named "doc" and their contents recursively
-REM Run this from the parent directory where you want the cleanup to start
+REM Recursively deletes all "doc" folders starting from the folder where this batch file is located
 
-echo Searching and deleting all "doc" folders...
-for /d /r %%i in (doc) do (
+setlocal
+
+REM Get the folder where the batch file is
+set "BASE_DIR=%~dp0"
+
+echo Searching recursively inside: %BASE_DIR%
+
+for /d /r "%BASE_DIR%" %%i in (*) do (
     if /i "%%~nxi"=="doc" (
         echo Deleting folder: %%i
         rmdir /s /q "%%i"
